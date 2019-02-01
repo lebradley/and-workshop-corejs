4cc6678790fb912a1ad2c835a781d8a5a5c50ac1
lbradley <lbradley@and.digital>
2019-02-01 14:47:11 +0000

    Make test work

--- sdk/helpers/userHelper.js
+++ sdk/helpers/userHelper.js
@@ -1,22 +1,23 @@
+const axios = require('axios');
+
 function getUserByUserName(username) {
-    return new Error('not implemented')
+  return new Error('not implemented');
 }
 
-function getUserByUserEmail(email) {
-    
-}
+function getUserByUserEmail(email) {}
 
-function getUserByUserId(id) {
-    
-} 
+function getUserByUserId(id) {}
 
 function getAllUsers() {
-    
+  return axios
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then(response => response)
+    .catch(error => console.log(error));
 }
 
 module.exports = {
-    getUserByUserName,
-    getUserByUserEmail,
-    getUserByUserId,
-    getAllUsers,
-}
+  getUserByUserName,
+  getUserByUserEmail,
+  getUserByUserId,
+  getAllUsers
+};
